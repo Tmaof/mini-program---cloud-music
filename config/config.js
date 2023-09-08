b1230bf1be4a5f91f14959ef8d5e031e98d84d59
MAOFU <2148334528@qq.com>
2023-09-08 09:14:53 +0800

    修复tab-bar 不能正确显示当前激活项的bug

--- config/config.js
+++ config/config.js
@@ -1,8 +1,5 @@
 export default {
-  // baseUrl: 'http://127.0.0.1:3000',
-  // baseUrl: 'https://cloud-music.maofu123.top',
-  // baseUrl: 'http://139.196.255.177:3434',
-  baseUrl: 'https://795z8353a3.zicp.fun',
+  baseUrl: 'http://127.0.0.1:3000',
   cookieKey: 'cookie', //cookie保存本地的key名称
   blogUrl: 'www.maofu123.top',
   searchHistoryKey: 'searchHistory'
